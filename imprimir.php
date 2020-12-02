5bae79df1d947bbb09134f5fa04fc3065b4fa70c
Roberto Pacheco <52426391+rpacheco2019@users.noreply.github.com>
2020-12-01 21:27:26 -0500

    liberacion 1.0

---
+++ imprimir.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<?php
+    include('xcrud/xcrud.php');
+?>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formato de impresión</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+        $db = Xcrud_db::get_instance();
+        $db->query("SELECT * FROM formato WHERE id =".$_GET['id']); // executes query, returns count of affected rows
+        $resultados = $db->row(); // loads results as list of arrays
+?>
+
+
+<div class="container-fluid" style="width: 85%;">
+	<br>
+<div class="row">
+    <div class="col">
+      <h2>ODP Lluvia de azúcar</h2>
+	  <br>
+	  <h5>Orden: [<?php echo $resultados['id']; ?>] - Cliente [<?php echo $resultados['nombreCliente'] ?>] </h5>
+      <h5>Dirección: <?php echo $resultados['direccionEntrega'] ?></h5>
+    </div>
+    <div class="col-md-auto">
+    </div>
+  </div>
+
+		<table class="table table-sm">
+		  <thead class="thead-dark">
+		    <tr>
+		      <th scope="col">Concepto:</th>
+		      <th scope="col">Descripción:</th>
+		      <th scope="col">Concepto:</th>
+		      <th scope="col">Descripción:</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		    <tr>
+		      <td class="table-active">Fecha compromiso</td>
+		      <td><?php echo $resultados['fechaEntrega']; ?></td>
+		      <td class="table-active">Fecha de producción</td>
+		      <td><?php echo $resultados['fechaProduccion'] ?></td>
+		    </tr>
+		    <tr>
+		      <td class="table-active">ODS Zoho:</td>
+		      <td><?php echo $resultados['ODS']; ?></td>
+		      <td class="table-active">PAX:</td>
+		      <td><?php echo  $resultados['pax'];?></td>
+		    </tr>
+		    <tr>
+		      <td class="table-active">Producto:</td>
+		      <td><?php echo $resultados['producto']?></td>
+		      <td class="table-active">Evento:</td>
+		      <td><?php echo $resultados['evento']; ?></td>
+		    </tr>
+		    <tr>
+		     <td class="table-active">Tema:</td>
+		      <td><?php echo $resultados['tema']; ?></td>
+		      <td class="table-active">Texto en diseño:</td>
+		      <td><?php echo $resultados['textoGeneral']; ?></td>
+		    </tr>
+		    <tr>
+		        <td class="table-active">Tipo de pastel:</td>
+		        <td><?php echo $resultados['tipoPastel']; ?></td>
+                <td class="table-active">Tipo de flor:</td>
+		        <td><?php echo $resultados['tipoFlor']; ?></td>
+		    </tr>
+			<tr>
+		        <td class="table-active">Tipo Evento</td>
+		        <td><?php echo $resultados['tipoEvento']; ?></td>
+                <td class="table-active">Detalle Flor:</td>
+		        <td><?php echo $resultados['descFlor']; ?></td>
+		    </tr>
+		    <tr>
+		      <td class="table-active">Estado</td>
+		      <td><?php echo $resultados['estado']; ?></td>
+		      <td class="table-active">Nota de pago:</td>
+		      <td><?php echo $resultados['notaInterna']; ?></td>
+		    </tr>
+		     </tbody>
+		</table>
+
+		<table class="table table-sm">
+			<thead class="thead-dark">
+			<tr>
+				<th>#</th>
+				<th>Altura</th>
+				<th>Pan</th>
+				<th>Relleno</th>
+				<th>Relleno 2</th>
+			</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td class="table-active">PAX 8</td>
+					<td><?php echo $resultados['alturaPiso0']; ?></td>
+					<td><?php echo $resultados['panPiso0'] ?></td>
+					<td><?php echo $resultados['rellenoPiso0'] ?></td>
+					<td><?php echo $resultados['relleno2Piso0'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 10</td>
+					<td><?php echo $resultados['alturaPiso1']; ?></td>
+					<td><?php echo $resultados['panPiso1'] ?></td>
+					<td><?php echo $resultados['rellenoPiso1'] ?></td>
+					<td><?php echo $resultados['relleno2Piso1'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 20</td>
+					<td><?php echo $resultados['alturaPiso2']; ?></td>
+					<td><?php echo $resultados['panPiso2'] ?></td>
+					<td><?php echo $resultados['rellenoPiso2'] ?></td>
+					<td><?php echo $resultados['relleno2Piso2'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 30</td>
+					<td><?php echo $resultados['alturaPiso3']; ?></td>
+					<td><?php echo $resultados['panPiso3'] ?></td>
+					<td><?php echo $resultados['rellenoPiso3'] ?></td>
+					<td><?php echo $resultados['relleno2Piso3'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 40</td>
+					<td><?php echo $resultados['alturaPiso4']; ?></td>
+					<td><?php echo $resultados['panPiso4'] ?></td>
+					<td><?php echo $resultados['rellenoPiso4'] ?></td>
+					<td><?php echo $resultados['relleno2Piso4'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 50</td>
+					<td><?php echo $resultados['alturaPiso5']; ?></td>
+					<td><?php echo $resultados['panPiso5'] ?></td>
+					<td><?php echo $resultados['rellenoPiso5'] ?></td>
+					<td><?php echo $resultados['relleno2Piso5'] ?></td>
+				</tr>
+
+			</tbody>
+			
+		</table>
+
+
+<p><b>Imagen de inspiración: </b> <?php ?></p>
+<img src="uploads/<?php echo $resultados['imagen'];?>" alt="" width="300" height="300">
+<hr>
+
+
+<p>Firma: <?php  ?> </p>
+Fecha de firma / Entregado: __________________________
+<br><br>
+	
+<table class="table table-sm">
+			<thead class="thead-dark">
+			<tr>
+				<th>#</th>
+				<th>Altura</th>
+				<th>Pan</th>
+				<th>Relleno</th>
+				<th>Relleno 2</th>
+			</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td class="table-active">PAX 8</td>
+					<td><?php echo $resultados['alturaPiso0']; ?></td>
+					<td><?php echo $resultados['panPiso0'] ?></td>
+					<td><?php echo $resultados['rellenoPiso0'] ?></td>
+					<td><?php echo $resultados['relleno2Piso0'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 10</td>
+					<td><?php echo $resultados['alturaPiso1']; ?></td>
+					<td><?php echo $resultados['panPiso1'] ?></td>
+					<td><?php echo $resultados['rellenoPiso1'] ?></td>
+					<td><?php echo $resultados['relleno2Piso1'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 20</td>
+					<td><?php echo $resultados['alturaPiso2']; ?></td>
+					<td><?php echo $resultados['panPiso2'] ?></td>
+					<td><?php echo $resultados['rellenoPiso2'] ?></td>
+					<td><?php echo $resultados['relleno2Piso2'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 30</td>
+					<td><?php echo $resultados['alturaPiso3']; ?></td>
+					<td><?php echo $resultados['panPiso3'] ?></td>
+					<td><?php echo $resultados['rellenoPiso3'] ?></td>
+					<td><?php echo $resultados['relleno2Piso3'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 40</td>
+					<td><?php echo $resultados['alturaPiso4']; ?></td>
+					<td><?php echo $resultados['panPiso4'] ?></td>
+					<td><?php echo $resultados['rellenoPiso4'] ?></td>
+					<td><?php echo $resultados['relleno2Piso4'] ?></td>
+				</tr>
+
+				<tr>
+					<td class="table-active">PAX 50</td>
+					<td><?php echo $resultados['alturaPiso5']; ?></td>
+					<td><?php echo $resultados['panPiso5'] ?></td>
+					<td><?php echo $resultados['rellenoPiso5'] ?></td>
+					<td><?php echo $resultados['relleno2Piso5'] ?></td>
+				</tr>
+
+			</tbody>
+			
+		</table>
+	
+
+</div>
+
+</body>
+</html>
+
+
+
+
---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+    session_start();
+    include('xcrud/xcrud.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Lluvia de azúcar 1.0</title>
+</head>
+ 
+<body>
+ 
+<?php
+    $xcrud= Xcrud::get_instance()->table('formato');
+    
+    /* Bloqueo de campos en Edit() */
+    $xcrud->fields('id',true);
+
+    /* Mapeo de columnas */
+    $xcrud->columns('id,ODS,nombreCliente,pax,producto,tema,estado,fechaProduccion,fechaEntrega');
+
+    /* Cambiar nombres de columnas y campos */
+    $xcrud->label('nombreCliente','Cliente');
+    $xcrud->label('notaInterna','Folio de Pago');
+    $xcrud->label('fechaEntrega','Fecha Compromiso');
+    $xcrud->label('fechaProduccion','Fecha de Produccion');
+    $xcrud->label('ODS','ODS ZOHO');
+    $xcrud->label('tipoEvento','Tipo de Evento');
+    $xcrud->label('evento','Nombre de Evento');
+    $xcrud->label('direccionEntrega','Dirección de Entrega');
+    $xcrud->label('tipoPastel','Tipo de pastel');
+    $xcrud->label('tipoFlor','Tipo de flor');
+    $xcrud->label('descFlor','Detalles de Flores');
+    $xcrud->label('textoGeneral','Texto a escribir en el pastel');
+
+    $xcrud->label('alturaPiso0','Altura PAX 8');
+    $xcrud->label('panPiso0','Pan PAX 8');
+    $xcrud->label('rellenoPiso0','Relleno PAX 8');
+    $xcrud->label('relleno2Piso0','Relleno 2 PAX 8');
+
+    $xcrud->label('alturaPiso1','Altura PAX 10');
+    $xcrud->label('panPiso1','Pan PAX 10');
+    $xcrud->label('rellenoPiso1','Relleno PAX 10');
+    $xcrud->label('relleno2Piso1','Relleno 2 PAX 10');
+
+    $xcrud->label('alturaPiso2','Altura PAX 20');
+    $xcrud->label('panPiso2','Pan PAX 20');
+    $xcrud->label('rellenoPiso2','Relleno PAX 20');
+    $xcrud->label('relleno2Piso2','Relleno 2 PAX 20');
+
+    $xcrud->label('alturaPiso3','Altura PAX 30');
+    $xcrud->label('panPiso3','Pan PAX 30');
+    $xcrud->label('rellenoPiso3','Relleno PAX 30');
+    $xcrud->label('relleno2Piso3','Relleno 2 PAX 30');
+
+    $xcrud->label('alturaPiso4','Altura PAX 40');
+    $xcrud->label('panPiso4','Pan PAX 40');
+    $xcrud->label('rellenoPiso4','Relleno PAX 40');
+    $xcrud->label('relleno2Piso4','Relleno 2 PAX 40');
+
+    $xcrud->label('alturaPiso5','Altura PAX 50');
+    $xcrud->label('panPiso5','Pan PAX 50');
+    $xcrud->label('rellenoPiso5','Relleno PAX 50');
+    $xcrud->label('relleno2Piso5','Relleno 2 PAX 50');
+
+    /* Tabs */
+    $xcrud->fields('fechaEntrega,fechaProduccion,notaInterna,ODS,estado', false, 'Programación');
+    $xcrud->fields('nombreCliente,evento,tipoEvento,direccionEntrega', false, 'Cliente');
+    $xcrud->fields('producto,pax,tema,tipoPastel,tipoFlor,descFlor,textoGeneral,imagen', false, 'Producto');
+    $xcrud->fields('alturaPiso0,panPiso0,rellenoPiso0,relleno2Piso0,alturaPiso1,panPiso1,rellenoPiso1,relleno2Piso1,alturaPiso2,panPiso2,rellenoPiso2,relleno2Piso2,alturaPiso3,panPiso3,rellenoPiso3,relleno2Piso3,alturaPiso4,panPiso4,rellenoPiso4,relleno2Piso4,alturaPiso5,panPiso5,rellenoPiso5,relleno2Piso5', false, 'Pisos(pastel)');
+    /* $xcrud->fields('alturaPiso1,panPiso1,rellenoPiso1,alturaPiso2,panPiso2,rellenoPiso2,alturaPiso3,panPiso3,rellenoPiso3,alturaPiso4,panPiso4,rellenoPiso4,alturaPiso5,panPiso5,rellenoPiso5', false, 'Pisos(pastel)'); */
+    
+
+    /* Cambiar nombre de la tabla */
+    $xcrud->table_name('Ordenes de producción');
+
+    /* Listas de campos */
+    $xcrud->change_type('producto','select','black,white',array('values'=>'Pastel,Cupcake,Galletas,Letras'));
+    $xcrud->change_type('tipoFlor','select','black,white',array('values'=>'Ninguna,Natural,Azucar'));
+    $xcrud->change_type('tipoPastel','select','black,white',array('values'=>'Crema,Fondant,Naked'));
+    $xcrud->change_type('estado','select','black,white',array('values'=>'Produciendo,Terminado,Entregado'));
+
+    $xcrud->change_type('tipoEvento','select','black,white',array('values'=>'Cumpleaños,Boda,XV Años,Bautizo,Comunión,Otros'));
+
+    $xcrud->change_type('alturaPiso0','select','black,white',array('values'=>'-,4",5",6",7",8",9",10"'));
+    $xcrud->change_type('alturaPiso1','select','black,white',array('values'=>'-,4",5",6",7",8",9",10"'));
+    $xcrud->change_type('alturaPiso2','select','black,white',array('values'=>'-,4",5",6",7",8",9",10"'));
+    $xcrud->change_type('alturaPiso3','select','black,white',array('values'=>'-,4",5",6",7",8",9",10"'));
+    $xcrud->change_type('alturaPiso4','select','black,white',array('values'=>'-,4",5",6",7",8",9",10"'));
+    $xcrud->change_type('alturaPiso5','select','black,white',array('values'=>'-,4",5",6",7",8",9",10"'));
+
+    $xcrud->change_type('panPiso0','select','black,white',array('values'=>'-,Falso,Vanilla,Marmoleado,Chocolate,Red Velvet,Arcoiris,Funfetti,Marmol Colores'));
+    $xcrud->change_type('panPiso1','select','black,white',array('values'=>'-,Falso,Vanilla,Marmoleado,Chocolate,Red Velvet,Arcoiris,Funfetti,Marmol Colores'));
+    $xcrud->change_type('panPiso2','select','black,white',array('values'=>'-,Falso,Vanilla,Marmoleado,Chocolate,Red Velvet,Arcoiris,Funfetti,Marmol Colores'));
+    $xcrud->change_type('panPiso3','select','black,white',array('values'=>'-,Falso,Vanilla,Marmoleado,Chocolate,Red Velvet,Arcoiris,Funfetti,Marmol Colores'));
+    $xcrud->change_type('panPiso4','select','black,white',array('values'=>'-,Falso,Vanilla,Marmoleado,Chocolate,Red Velvet,Arcoiris,Funfetti,Marmol Colores'));
+    $xcrud->change_type('panPiso5','select','black,white',array('values'=>'-,Falso,Vanilla,Marmoleado,Chocolate,Red Velvet,Arcoiris,Funfetti,Marmol Colores'));
+
+    $xcrud->change_type('rellenoPiso0','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('relleno2Piso0','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('rellenoPiso1','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('relleno2Piso1','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('rellenoPiso2','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('relleno2Piso2','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('rellenoPiso3','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('relleno2Piso3','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('rellenoPiso4','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('relleno2Piso4','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('rellenoPiso5','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+    $xcrud->change_type('relleno2Piso5','select','black,white',array('values'=>'-,Ganache,Nutella,Chocolate Blanco,Dulce de leche,Philadelphia,Fresas con crema,Brigadeiro'));
+
+    /* Imagen */
+    $xcrud->change_type('imagen', 'image', '', array('not_rename'=>true));
+
+    /* Pruebas  */
+    $xcrud->button('imprimir.php?id={id}');
+
+    /* Seek and destroy */
+    echo $xcrud;
+    /* echo $xcrud->render('create'); */
+
+?>
+ 
+</body>
+</html>
